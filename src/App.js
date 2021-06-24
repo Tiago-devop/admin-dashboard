8d702b1947c529401651dd5f19b5e711e2fa32d6
Tiago-devop <tiagodev@protonmail.com>
2021-06-24 12:01:09 -0300

    adding sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
 
 function App() {
   return (
     <div>
       <Topbar />
+      <div className="container">
+        <Sidebar />
+      </div>
     </div>
   );
 }
