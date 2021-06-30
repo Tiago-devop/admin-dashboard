547411ac6e3540d2368e635b8f4c7005021a6b47
Tiago-devop <tiagodev@protonmail.com>
2021-06-30 19:16:32 -0300

    adding chart

---
+++ src/components/chart/Chart.jsx
@@ -0,0 +1,56 @@
+import "./Chart.css";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
+export default function Chart() {
+  const data = [
+    {
+      name: "Page A",
+      "Active User": 4000,
+    },
+    {
+      name: "Page B",
+      "Active User": 3000,
+    },
+    {
+      name: "Page C",
+      "Active User": 2000,
+    },
+    {
+      name: "Page D",
+      "Active User": 2780,
+    },
+    {
+      name: "Page E",
+      "Active User": 1890,
+    },
+    {
+      name: "Page F",
+      "Active User": 2390,
+    },
+    {
+      name: "Page G",
+      "Active User": 3490,
+    },
+  ];
+
+  return (
+    <div className="chart">
+      <h3 className="chartTitle">User Analytics</h3>
+      <ResponsiveContainer width="100%" aspect={4 / 1}>
+        <LineChart data={data}>
+          <XAxis dataKey="name" stroke="#5550bd" />
+          <Line type="monotone" dataKey="Active User" stroke="#5550bd" />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  );
+}
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,10 +1,12 @@
 import "./Home.css";
 import FeaturedInfo from "../featuredInfo/FeaturedInfo";
+import Chart from "../chart/Chart";
 
 export default function Home() {
   return (
     <div className="home">
       <FeaturedInfo />
+      <Chart />
     </div>
   );
 }
