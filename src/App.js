9488123296834825172cfadc011350b12283e218
Tiago-devop <tiagodev@protonmail.com>
2021-07-02 15:56:32 -0300

    adding mat. ui data-grid and styling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import "./App.css";
 import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
+import UserList from "./pages/userList/UserList";
 import Home from "./pages/home/Home";
 
 function App() {
   return (
-    <div>
+    <Router>
       <Topbar />
       <div className="container">
         <Sidebar />
-        <Home />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/users">
+            <UserList />
+          </Route>
+        </Switch>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- src/pages/userList/UserList.jsx
+++ src/pages/userList/UserList.jsx
@@ -1,9 +1,139 @@
-import './UserList.css';
+import "./UserList.css";
+import { DataGrid } from "@material-ui/data-grid";
 
 export default function UserList() {
+  const columns = [
+    { field: "id", headerName: "ID", width: 90 },
+    {
+      field: "user",
+      headerName: "User",
+      width: 200,
+      renderCell: (params) => {
+        return (
+          <div className="userListUser">
+            <img className="userListImg" src={params.row.avatar} alt="" />
+            {params.row.username}
+          </div>
+        );
+      },
+    },
+    { field: "email", headerName: "Email", width: 200 },
+    {
+      field: "status",
+      headerName: "Status",
+      width: 130,
+    },
+    {
+      field: "transaction",
+      headerName: "Transaction Volume",
+      width: 160,
+    },
+  ];
+
+  const rows = [
+    {
+      id: 1,
+      username: "Maria catrina",
+      avatar:
+        "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?cs=srgb&dl=pexels-moose-photos-1036623.jpg&fm=jpg",
+      email: "Maria@gmail.com",
+      status: "active",
+      transaction: "$120.00",
+    },
+    {
+      id: 2,
+      username: "Maria catrina",
+      avatar:
+        "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?cs=srgb&dl=pexels-moose-photos-1036623.jpg&fm=jpg",
+      email: "Maria@gmail.com",
+      status: "active",
+      transaction: "$120.00",
+    },
+    {
+      id: 3,
+      username: "Maria catrina",
+      avatar:
+        "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?cs=srgb&dl=pexels-moose-photos-1036623.jpg&fm=jpg",
+      email: "Maria@gmail.com",
+      status: "active",
+      transaction: "$120.00",
+    },
+    {
+      id: 4,
+      username: "Maria catrina",
+      avatar:
+        "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?cs=srgb&dl=pexels-moose-photos-1036623.jpg&fm=jpg",
+      email: "Maria@gmail.com",
+      status: "active",
+      transaction: "$120.00",
+    },
+    {
+      id: 5,
+      username: "Maria catrina",
+      avatar:
+        "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?cs=srgb&dl=pexels-moose-photos-1036623.jpg&fm=jpg",
+      email: "Maria@gmail.com",
+      status: "active",
+      transaction: "$120.00",
+    },
+    {
+      id: 6,
+      username: "Maria catrina",
+      avatar:
+        "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?cs=srgb&dl=pexels-moose-photos-1036623.jpg&fm=jpg",
+      email: "Maria@gmail.com",
+      status: "active",
+      transaction: "$120.00",
+    },
+    {
+      id: 7,
+      username: "Maria catrina",
+      avatar:
+        "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?cs=srgb&dl=pexels-moose-photos-1036623.jpg&fm=jpg",
+      email: "Maria@gmail.com",
+      status: "active",
+      transaction: "$120.00",
+    },
+    {
+      id: 8,
+      username: "Maria catrina",
+      avatar:
+        "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?cs=srgb&dl=pexels-moose-photos-1036623.jpg&fm=jpg",
+      email: "Maria@gmail.com",
+      status: "active",
+      transaction: "$120.00",
+    },
+    {
+      id: 9,
+      username: "Maria catrina",
+      avatar:
+        "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?cs=srgb&dl=pexels-moose-photos-1036623.jpg&fm=jpg",
+      email: "Maria@gmail.com",
+      status: "active",
+      transaction: "$120.00",
+    },
+    {
+      id: 10,
+      username: "Maria catrina",
+      avatar:
+        "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?cs=srgb&dl=pexels-moose-photos-1036623.jpg&fm=jpg",
+      email: "Maria@gmail.com",
+      status: "active",
+      transaction: "$120.00",
+    },
+    {
+      id: 11,
+      username: "Maria catrina",
+      avatar:
+        "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?cs=srgb&dl=pexels-moose-photos-1036623.jpg&fm=jpg",
+      email: "Maria@gmail.com",
+      status: "active",
+      transaction: "$120.00",
+    },
+  ];
   return (
     <div className="userList">
-      UserList
+      <DataGrid rows={rows} columns={columns} pageSize={5} checkboxSelection />
     </div>
-  )
+  );
 }
