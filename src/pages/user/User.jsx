db4c627ca4779f989190e19be07438378f4bde30
Tiagodev <tiagoramosvasconcelos.work@gmail.com>
2021-07-12 11:50:43 -0700

    styling edit form

--- src/pages/user/User.jsx
+++ src/pages/user/User.jsx
@@ -4,6 +4,7 @@ import {
   MailOutline,
   PermIdentity,
   PhoneAndroid,
+  Publish,
 } from "@material-ui/icons";
 import "./User.css";
 
@@ -37,6 +38,7 @@ export default function User() {
               <CalendarToday className="userShowIcon" />
               <span className="userShowInfoTitle">10.12.1999</span>
             </div>
+            <span className="userShowTitle">Contact Details</span>
             <div className="userShowInfo">
               <PhoneAndroid className="userShowIcon" />
               <span className="userShowInfoTitle">+1 123 456 67</span>
@@ -51,7 +53,66 @@ export default function User() {
             </div>
           </div>
         </div>
-        <div className="userUpdate"></div>
+        <div className="userUpdate">
+          <span className="userUpdateTitle">Edit</span>
+          <form className="userUpdateForm">
+            <div className="userUpdateLeft">
+              <div className="userUpdateItem">
+                <label>Username</label>
+                <input
+                  type="text"
+                  placeholder="annaklaus76"
+                  className="userUpdateInput"
+                />
+              </div>
+              <div className="userUpdateItem">
+                <label>Full Name</label>
+                <input
+                  type="text"
+                  placeholder="Anna Klaus"
+                  className="userUpdateInput"
+                />
+              </div>
+              <div className="userUpdateItem">
+                <label>Email</label>
+                <input
+                  type="text"
+                  placeholder="annaklaus76@gmail.com"
+                  className="userUpdateInput"
+                />
+              </div>
+              <div className="userUpdateItem">
+                <label>Phone</label>
+                <input
+                  type="text"
+                  placeholder="+1 123 456 67"
+                  className="userUpdateInput"
+                />
+              </div>
+              <div className="userUpdateItem">
+                <label>Address</label>
+                <input
+                  type="text"
+                  placeholder="New York | USA"
+                  className="userUpdateInput"
+                />
+              </div>
+            </div>
+            <div className="userUpdateRight">
+              <div className="userUpdateUpload">
+                <img
+                  className="userUpdateImg"
+                  src="https://images.pexels.com/photos/1035685/pexels-photo-1035685.jpeg?cs=srgb&dl=pexels-anastasiya-lobanovskaya-1035685.jpg&fm=jpg"
+                  alt=""
+                />
+                <label htmlFor="file">
+                  <Publish />
+                </label>
+                <input type="file" id="file" style={{ display: "none" }} />
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
