1a6e4f4778b57f19bf91070e8647499c036e9d91
Tiagodev <tiagoramosvasconcelos.work@gmail.com>
2021-07-12 10:36:28 -0700

    styling edit user

--- src/pages/user/User.jsx
+++ src/pages/user/User.jsx
@@ -1,3 +1,10 @@
+import {
+  CalendarToday,
+  LocationSearching,
+  MailOutline,
+  PermIdentity,
+  PhoneAndroid,
+} from "@material-ui/icons";
 import "./User.css";
 
 export default function User() {
@@ -20,7 +27,29 @@ export default function User() {
               <span className="userShowUserTitle">Software Engineer</span>
             </div>
           </div>
-          <div className="userShowBottom"></div>
+          <div className="userShowBottom">
+            <span className="userShowTitle">Account Details</span>
+            <div className="userShowInfo">
+              <PermIdentity className="userShowIcon" />
+              <span className="userShowInfoTitle">annaklaus76</span>
+            </div>
+            <div className="userShowInfo">
+              <CalendarToday className="userShowIcon" />
+              <span className="userShowInfoTitle">10.12.1999</span>
+            </div>
+            <div className="userShowInfo">
+              <PhoneAndroid className="userShowIcon" />
+              <span className="userShowInfoTitle">+1 123 456 67</span>
+            </div>
+            <div className="userShowInfo">
+              <MailOutline className="userShowIcon" />
+              <span className="userShowInfoTitle">annaklaus76@gmail.com</span>
+            </div>
+            <div className="userShowInfo">
+              <LocationSearching className="userShowIcon" />
+              <span className="userShowInfoTitle">New York | USA</span>
+            </div>
+          </div>
         </div>
         <div className="userUpdate"></div>
       </div>
